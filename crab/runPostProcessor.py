9c79d3612ec3e1064c3cb89b294874cb7b2d1cdf
soumyadipbarman <barmansoumyadip@gmail.com>
2020-10-25 13:36:08 +0100

    update runPostProcessor.py

--- crab/runPostProcessor.py
+++ crab/runPostProcessor.py
@@ -16,8 +16,6 @@ from PhysicsTools.NanoAODTools.postprocessing.examples.skimmer import *         
 
 name= 'JetCharge_skimmer'
 
-#preselection = "((HLT_PFJet60==1 && Jet_pt[0] > 81 ) || (HLT_PFJet80==1 && Jet_pt[0] > 103) || (HLT_PFJet140==1 && Jet_pt[0] > 191 ) || (HLT_PFJet200==1 && Jet_pt[0] > 258) || (HLT_PFJet260==1 && Jet_pt[0] > 333 ) || (HLT_PFJet320==1 && Jet_pt[0] > 363) || (HLT_PFJet400==1 && Jet_pt[0] > 458 ) || (HLT_PFJet450==1 && Jet_pt[0] > 493) || (HLT_PFJet500==1 && Jet_pt[0] > 580))"# || (HLT_PFJet550==1))"
-
 
 #p=PostProcessor(".",files,"Jet_pt>400","keep_and_drop.txt", modules=[jmeCorrections2018C_DATA_AK4CHS(), jetskimmer()],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis())
 #p=PostProcessor(".",files,"Jet_pt>30", modules=[jmeCorrections2018_MC_AK4CHS(), MCeventselectionTest()],)
